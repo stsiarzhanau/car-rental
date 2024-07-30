5c441dfd538c9fb7ae5bbaf511d448bbc69a2e53
Aliaksandr Stsiarzhanau <sterzhanoff@gmail.com>
2024-07-30 07:47:16 +0200

    Disable msw warnings for unhandled requests

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,9 @@ import App from './App.tsx';
 
 async function enableMocking() {
   const { worker } = await import('./mocks/browser');
-  return worker.start();
+  return worker.start({
+    onUnhandledRequest: 'bypass',
+  });
 }
 
 /* https://mswjs.io/docs/integrations/browser */
